c1c09ba75046b66f6dcdf40ec755d7f41f9a9663
anhquan2006 <aquan755@gmail.com>
2025-09-24 19:52:27 +0700

    Add files via upload

---
+++ Bai33.java
@@ -0,0 +1,9 @@
+public class Bai33 {
+    public static void main(String[] args) {
+        // Vòng lặp for sẽ lặp lại 20 lần
+        for (int i = 0; i < 20; i++) {
+            // In ra chuỗi "PTIT" và xuống dòng
+            System.out.println("PTIT");
+        }
+    }
+}
